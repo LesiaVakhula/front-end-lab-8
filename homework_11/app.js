e20e4d3554fd88614a1fe4adcc10ec3a83d4d9ff
LesiaVakhula <vakhulalesia@gmail.com>
2018-02-24 17:58:11 +0200

    formatted app.js and index.html

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,41 +1,39 @@
 var rootNode = document.getElementById("root");
 
 function createElem(arr) {
-    var folder = document.createElement('ul');
+    let folder = document.createElement('ul');
     if (arr === structure) {
         folder.className = 'block';
 
     } else {
         folder.className = 'hide';
     }
-    for (let i = 0, l = arr.length; i < l; i++) {
 
+    for (let i = 0, l = arr.length; i < l; i++) {
         let subFolder = document.createElement('li');
 
         function show() {
             let child = this.children;
-            child[0].innerHTML = '<i class="material-icons">folder_open</i>' + arr[i].title;
-            child[1].className = 'block';
-            event.stopPropagation();
-            this.removeEventListener('click', show);
-            this.addEventListener('click', hide)
+                child[0].innerHTML = '<i class="material-icons">folder_open</i>' + arr[i].title;
+                child[1].className = 'block';
+                event.stopPropagation();
+                this.removeEventListener('click', show);
+                this.addEventListener('click', hide)
         }
         function hide() {
             if (event.target.className === 'stop') {
                 event.target.removeEventListener('click', hide);
                 return
             }
-            console.log(event.target);
             let child = this.children;
-            child[1].className = 'hide';
-            child[0].innerHTML = '<i class="material-icons ">folder</i>' + arr[i].title;
-            this.removeEventListener('click', hide);
-            this.addEventListener('click', show);
-            event.stopPropagation();
+                child[1].className = 'hide';
+                child[0].innerHTML = '<i class="material-icons ">folder</i>' + arr[i].title;
+                this.removeEventListener('click', hide);
+                this.addEventListener('click', show);
+                event.stopPropagation();
         }
 
-        if (arr[i].folder === true) {
-
+        if (arr[i].folder) {
             subFolder.innerHTML = '<p><i class="material-icons ">folder</i>' + arr[i].title + '</p>';
             subFolder.addEventListener('click', show);
         } else {
@@ -47,13 +45,12 @@ function createElem(arr) {
         }
 
         if (arr[i].children === null || arr[i].children === false) {
-            let emptyUl = document.createElement('ul');
-            emptyUl.className = 'hide';
-            let emptyLi = document.createElement('li');
-            emptyLi.innerHTML = '<i class="stop">Folder is empty</i>';
-            emptyUl.appendChild(emptyLi);
-            subFolder.appendChild(emptyUl);
-
+            let emptyUl = document.createElement('ul'),
+                emptyLi = document.createElement('li');
+                emptyUl.className = 'hide';
+                emptyLi.innerHTML = '<i class="stop">Folder is empty</i>';
+                emptyUl.appendChild(emptyLi);
+                subFolder.appendChild(emptyUl);
         }
         folder.appendChild(subFolder);
     }
