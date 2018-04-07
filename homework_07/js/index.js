1683f352d068d6e13ef52c7b1615832546a8074a
LesiaVakhula <vakhulalesia@gmail.com>
2018-04-08 02:17:49 +0300

    edited index.js and style.js

--- homework_07/js/index.js
+++ homework_07/js/index.js
@@ -18,7 +18,13 @@ $(document).ready(function () {
             }
         }
     }
+    $("h1").after("<button>");
+    $("button").text("Restart").click(function () {
+        location.reload();
+    });
 
+    let winner = $("<div class='winner'>");
+    $(".player-black").before(winner);
     $("<div class='main-board'>").appendTo(".main");
 
     for (let i = 0; i < 15; i++) {
@@ -40,12 +46,9 @@ $(document).ready(function () {
     $(".item-circle").one("click", function (e) {
         let circle = e.target;
         let arr = $(".players div");
-        let parent = $(circle).parent();
-        let arrWite = [];
-        let arrBlack = [];
-        let mainParent = $(".main-circle").children();
-
-        arr.toggleClass("active");
+        let arrHorizontal =[];
+        let arrVertical =[];
+            arr.toggleClass("active");
 
         for (let i = 0, l = arr.length; i < l; i++) {
             if (arr.eq(i).is(".active")) {
@@ -53,80 +56,170 @@ $(document).ready(function () {
                     $(circle).addClass("active-white");
                     $(circle).text("1").css({color: "transparent",fontSize:0});
                     $(circle).removeClass("active-hover")
-                } else if (arr.is(".player-white")) {
+                } else if (arr.eq(i).is(".player-white")) {
                     $(circle).addClass("active-black");
-                    $(circle).removeClass("active-hover")
+                    $(circle).removeClass("active-hover");
                     $(circle).text("2").css({color: "transparent",fontSize:0});
                 }
             }
         }
+        for(let i=1; i<=2; i++){
+            if($(circle).text() == i){
+                let transitional = $(circle);
+                arrHorizontal = arrHorizontal.concat(Array.from(transitional));
+                for(let j=0; j<4; j++){
+                    transitional = transitional.next();
+                        if(transitional.text() == i){
+                            let item = Array.from(transitional);
+                            arrHorizontal = arrHorizontal.concat(item);
+                        }else {
+                            transitional = $(circle);
+                            break;
+                        }
+                };
 
-        function verify(item, itemNext) {
-            if (item.text() === "1" && itemNext.text() === "1") {
-                let itemWite = Array.from(item);
-                    arrWite = arrWite.concat(itemWite);
-                    if (arrWite.length === 3) {
-                        let winner = $("<div class='winner'>").text("White is a winner!!!");
-                            arrWite = arrWite.concat(Array.from(itemNext));
-                            $(".player-black").before(winner);
-                            $(".item-circle").off("click");
-                            for (let j = 0; j < arrWite.length; j++) {
-                                $(arrWite[j]).css({background: "red"});
+                for(let j = 0; j<4; j++){
+                    transitional = transitional.prev();
+                    if($(transitional).text() == i){
+                        let item = Array.from(transitional);
+                        arrHorizontal = arrHorizontal.concat(item);
+                    }else {
+                        break;
+                    }
+                }
+            }
+        }
+        if(arrHorizontal.length>3){
+            $(arrHorizontal).css({background: "red"});
+            $(".item-circle").off("click");
+            if($(arrHorizontal[0]).text() === "1"){
+                $(".winner").text("White is a winner");
+            }else {
+                $(".winner").text("Black is a winner");
+            }
+        }
+
+        for(let i=1; i<=2; i++) {
+            if ($(circle).text() == i) {
+                let parent = $(circle).parent();
+                let index = Array.from(parent.children()).indexOf(circle);
+                arrVertical = arrVertical.concat(circle);
+                for (let j = 0; j < 4; j++) {
+                    parent = parent.next();
+                    let child = $(parent).children()[index];
+                    if ($(child).text() == i) {
+                        let nextRowChild = Array.from($(child));
+                        arrVertical = arrVertical.concat(nextRowChild);
+                    } else {
+                        break;
+                    }
+                }
+                let parentPrev = $(circle).parent();
+                for (let k = 0; k < 4; k++) {
+                    parentPrev = parentPrev.prev();
+                    let childPrev = $(parentPrev).children()[index];
+                    if ($(childPrev).text() == i) {
+                        let prevRowChild = Array.from($(childPrev));
+                        arrVertical = arrVertical.concat(prevRowChild);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+            if (arrVertical.length > 3) {
+                $(arrVertical).css({background: "red"});
+                $(".item-circle").off("click");
+                    if($(arrVertical[0]).text() === "1"){
+                        $(".winner").text("White is a winner");
+                    }else {
+                        $(".winner").text("Black is a winner");
+                    }
+            }
+            let arrDiagonalRight = [];
+            for (let i = 1; i <= 2; i++) {
+                if ($(circle).text() == i) {
+                    let parent = $(circle).parent();
+                    let index = Array.from(parent.children()).indexOf(circle);
+                    let indexNext = index;
+                        arrDiagonalRight = arrDiagonalRight.concat(circle);
+                    for (let j = 0; j < 4; j++) {
+                        parent = parent.next();
+                        indexNext++;
+                        let child = $(parent).children()[indexNext];
+
+                        if ($(child).text() == i) {
+                            let nextRowChild = Array.from($(child));
+                            arrDiagonalRight = arrDiagonalRight.concat(nextRowChild);
+                        } else {
+                            break;
+                        }
+                    }
+                    let parentPrev = $(circle).parent();
+                    let indexPrev = index;
+                    for (let k = 0; k < 4; k++) {
+                        parentPrev = parentPrev.prev();
+                        indexPrev--;
+                        let childPrev = $(parentPrev).children()[indexPrev];
+                        if ($(childPrev).text() == i) {
+                            let prevRowChild = Array.from($(childPrev));
+                            arrDiagonalRight = arrDiagonalRight.concat(prevRowChild);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+            if (arrDiagonalRight.length > 3) {
+                $(arrDiagonalRight).css({background: "red"});
+                $(".item-circle").off("click");
+                if($(arrDiagonalRight[0]).text() === "1"){
+                    $(".winner").text("White is a winner");
+                }else {
+                    $(".winner").text("Black is a winner");
+                }
+            }
+            let arrDiagonalLeft = [];
+            for (let i = 1; i <= 2; i++) {
+                if ($(circle).text() == i) {
+                    let parent = $(circle).parent();
+                    let index = Array.from(parent.children()).indexOf(circle);
+                    arrDiagonalLeft = arrDiagonalLeft.concat(circle);
+                    let indexNext = index;
+                    for (let j = 0; j < 4; j++) {
+                        parent = parent.next();
+                        indexNext--;
+                        let child = $(parent).children()[indexNext];
+                            if ($(child).text() == i) {
+                                let nextRowChild = Array.from($(child));
+                                arrDiagonalLeft = arrDiagonalLeft.concat(nextRowChild);
+                            } else {
+                                break;
                             }
                     }
-            } else if (item.text() === "1" && itemNext.text() !== "1") {
-                arrWite = [];
-            } else if (item.text() === "2" && itemNext.text() === "2") {
-                let itemBlack = Array.from(item);
-                    arrBlack = arrBlack.concat(itemBlack);
-                    if (arrBlack.length === 3) {
-                        let winner = $("<div class='winner'>").text("Black is a winner!!!");
-                            arrBlack = arrBlack.concat(Array.from(itemNext));
-                            $(".player-black").before(winner);
-                            $(".item-circle").off("click");
-                            for (let k = 0; k < arrBlack.length; k++) {
-                                $(arrBlack[k]).css({background: "red"});
+                    let parentPrev = $(circle).parent();
+                    let indexPrev = index;
+                        for (let k = 0; k < 4; k++) {
+                            parentPrev = parentPrev.prev();
+                            indexPrev++;
+                            let childPrev = $(parentPrev).children()[indexPrev];
+                            if ($(childPrev).text() == i) {
+                                let prevRowChild = Array.from($(childPrev));
+                                arrDiagonalLeft = arrDiagonalLeft.concat(prevRowChild);
+                            } else {
+                                break;
                             }
-                    }
-            } else if (item.text() === "2" && itemNext.text() !== "2") {
-                arrBlack = [];
+                        }
+                }
             }
-        }
-
-        for (let i = 0; i < 16; i++) {
-            let par = $($(parent).children()[i]);
-            let parNext = par.next();
-                verify(par, parNext);
-        }
-
-        for (let j = 0; j < 16; j++) {
-            let parSibl = $(mainParent[j]);
-
-            for (let i = 0; i < 16; i++) {
-                let parSiblChild = $(parSibl.children()[i]);
-                let parSiblChildNext = $(parSibl.next().children()[i]);
-                    verify(parSiblChild, parSiblChildNext);
+            if (arrDiagonalLeft.length > 3) {
+                $(arrDiagonalLeft).css({background: "red"});
+                if($(arrDiagonalLeft[0]).text() === "1"){
+                    $(".winner").text("White is a winner");
+                }else {
+                    $(".winner").text("Black is a winner");
+                }
+                $(".item-circle").off("click");
             }
-        }
-
-        for (let j = 0; j < 16; j++) {
-            let parSibl = $(mainParent[j]);
-
-            for (let i = 0; i < 16; i++) {
-                let parSiblChild = $(parSibl.children()[i]);
-                let parSiblChildNext = $(parSibl.next().children()[i + 1]);
-                    verify(parSiblChild, parSiblChildNext);
-            }
-        }
-
-        for (let j = 0; j < 16; j++) {
-            let parSibl = $(mainParent[j]);
-
-            for (let i = 0; i < 16; i++) {
-                let parSiblChild = $(parSibl.children()[i]);
-                let parSiblChildPrev = $(parSibl.next().children()[i - 1]);
-                    verify(parSiblChild, parSiblChildPrev);
-            }
-        }
-    })
-});
+    });
+})
